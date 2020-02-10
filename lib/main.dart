2ca140f146e1d5d06b81eeb281ee03053481bf40
ajay <ajaysingh56656@gmail.com>
2020-02-10 15:03:23 +0530

    Add Hello File

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'Flutter Demo',
+      title: 'Flutter Deo',
       theme: ThemeData(
         // This is the theme of your application.
         //
